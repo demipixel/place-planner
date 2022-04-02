6ae4a43c7429c8f88a05228ead96b42e19a0aa30
Lucas Simon <luke5227@gmail.com>
2022-04-01 21:00:20 -0700

    Add support for https

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { getCurrentImage } from './get-image';
 import path from 'path';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 
@@ -13,9 +16,35 @@ app.use('/image', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(2995, () => {
-  console.log('Listening on port 2995');
+const PORT = process.env.NODE_ENV != 'production' ? 2995 : 443;
+
+const certPath = '/etc/letsencrypt/live/plan.place/fullchain.pem';
+const keyPath = '/etc/letsencrypt/live/plan.place/privkey.pem';
+const server = (
+  process.env.NODE_ENV == 'production'
+    ? https.createServer(
+        {
+          cert: fs.existsSync(certPath) ? fs.readFileSync(certPath) : undefined,
+          key: fs.existsSync(keyPath) ? fs.readFileSync(keyPath) : undefined,
+        },
+        app,
+      )
+    : http.createServer(app)
+).listen(PORT, () => {
+  console.log('Server live on port ' + PORT);
 });
+
+function requireHTTPS(req: any, res: any, next: any) {
+  if (!req.secure && process.env.NODE_ENV == 'production') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+if (process.env.NODE_ENV == 'production') {
+  app.listen(80);
+  app.use(requireHTTPS);
+}
 
 function refreshImageCache() {
   getCurrentImage()
